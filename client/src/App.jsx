08585fed686542cc89c3a89b201764719e41f72d
Dean14692-prog <dennismuasyangui@gmail.com>
2025-07-16 16:27:17 +0300

    FE

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,10 @@
 
 import './App.css'
+import Carousel from './components/carousel';
 import Footer from './components/footer';
 import Herossection from './components/herossection';
 import Navbar from './components/navbar';
-import FloatingDockDemo  from './components/navbar';
+import BackgroundGradientDemo from './components/projectpreview';
 import AnimatedTestimonialsDemo from './components/testimonials'
 
 function App() {
@@ -14,6 +15,8 @@ function App() {
       <div>
         <Navbar />
         <Herossection />
+        <Carousel />
+        <BackgroundGradientDemo />
         <AnimatedTestimonialsDemo />
         <Footer />
       </div>
