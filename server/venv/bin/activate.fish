3a51d29f0e6a579f50a6d3b8e305d3760d696011
nosmall <elvisotieno24@gmail.com>
2025-07-20 22:18:26 +0300

    removes filesa

--- server/venv/bin/activate.fish
+++ server/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV /home/elvis/Desktop/phase5/Moringa-Marketplace/server/venv
+set -gx VIRTUAL_ENV /home/elvis/Desktop/phase5/revised/Moringa-Marketplace/server/venv
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/"bin $PATH
